16932ef242c79986d8324269119254ab2f9b5a6d
Marius Sarca <marius.sarca@gmail.com>
2013-11-15 01:00:14 +0200

    Important changes

--- Connection.php
+++ Connection.php
@@ -1,425 +1,227 @@
 <?php
-/* ===========================================================================
- * Opis Project
- * http://opis.io
- * ===========================================================================
- * Copyright 2013 Marius Sarca
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ============================================================================ */
 
 namespace Opis\Database;
 
-use PDO;
 use Closure;
-use PDOException;
-use RuntimeException;
-use Opis\Database\SQL\Compiler;
+use PDO;
 
 class Connection
 {
-
-    /** @var    \PDO    PDO object. */
-    protected $pdo;
-
-    /** @var    string  Database name. */
+    
+    protected static $connections = array();
+    
+    protected static $compilers = array();
+    
+    protected static $defaultConnection = null;
+    
+    protected $username = null;
+    
+    protected $password = null;
+    
+    protected $log = false;
+    
+    protected $queries = array();
+    
+    protected $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_STRINGIFY_FETCHES => false,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    );
+    
+    protected $prefix;
+    
     protected $name;
     
-    /** @var    \Opis\Database\SQL\Compiler  Compiler. */
-    protected $compiler;
-
-    /** @var    boolean Enable log. */
-    protected $enableLog;
-
-    /** @var    array   Query log. */
-    protected $log = array();
+    protected $properties = array();
     
-    /**
-     * Constructor
-     *
-     * @access  public
-     * @param   string  $name   Connection name.
-     * @param   array   $config Connection configuration.
-     */
-    
-    public function __construct(Compiler $compiler, ConnectionInfo $info)
+    public function __construct($name, $prefix)
     {
-        
-        $this->name = $info->name();
-        
-        $this->enableLog = $info->logQueries();
-        
-
-        $options = array(
-            PDO::ATTR_PERSISTENT => $info->persistent(),
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-            PDO::ATTR_STRINGIFY_FETCHES => false,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        );
-        
-        try
-        {
-            $this->pdo = new PDO($info->dsn(), $info->username(), $info->password(), $options);
-        }
-        catch(PDOException $e)
-        {
-            throw new RuntimeException(vsprintf("%s(): Failed to connect to the '%s' database. %s", array(__METHOD__, $this->name, $e->getMessage())));
-        }
-
-        $queries = $info->queries();
-        
-        if(!empty($queries))
-        {
-            foreach($queries as $query)
-            {
-                $this->pdo->exec($query);
-            }
-        }
-        
-        $this->compiler = $compiler;
+        $this->prefix = $prefix;
     }
     
-    /**
-     * Returns the connection name.
-     *
-     * @access public
-     * @return string
-     */
-
-    public function getName()
+    public function name()
     {
-        return $this->name;
+        return $name;
     }
-
-    /**
-     * Returns the PDO instance.
-     *
-     * @access public
-     * @return \PDO
-     */
-
-    public function getPDO()
+    
+    public function enableLog($value = true)
     {
-        return $this->pdo;
+        $this->log = $value;
     }
-
-    /**
-     * Returns the driver name.
-     *
-     * @access public
-     * @return string
-     */
-
-    public function getDriver()
-    {
-        return $this->driver;
-    }
-
-    /**
-     * Returns the compiler name.
-     *
-     * @access public
-     * @return string
-     */
-
-    public function getCompiler()
-    {
-        return $this->compiler;
-    }
-
-    /**
-     * Enables the query log.
-     *
-     * @access public
-     */
-
-    public function enableLog()
-    {
-        $this->enableLog = true;
-    }
-
-    /**
-     * Disables the query log.
-     *
-     * @access public
-     */
-
-    public function disableLog()
-    {
-        $this->enableLog = false;
-    }
-
-    /**
-     * Replace placeholders with parameteters.
-     *
-     * @access protected
-     * @param string $query SQL query
-     * @param array $params Query paramaters
-     * @return string
-     */
-
-    protected function replaceParams($query, array $params)
-    {
-        $pdo = $this->pdo;
-        return preg_replace_callback('/\?/', function($matches) use (&$params, $pdo){
-            $param = array_shift($params);
-            return (is_int($param) || is_float($param)) ? $param : $pdo->quote(is_object($param) ? get_class($param) : $param);
-        }, $query);
-    }
-
-    /**
-     * Adds a query to the query log.
-     *
-     * @access protected
-     * @param string $query SQL query
-     * @param array $params Query parameters
-     * @param int $start Start time in microseconds
-     */
-
-    protected function log($query, array $params, $start)
-    {
-        $pdo = $this->pdo;
-        $time = microtime(true) - $start;
-        $query = $this->replaceParams($query, $params);
-        $this->log[] = compact('query', 'time');
-    }
-
-    /**
-     * Returns the query log for the connection.
-     *
-     * @access public
-     * @return array
-     */
-
-    public function getLog()
+    
+    public function logQueries()
     {
         return $this->log;
     }
-
-    /**
-     * Prepares a query.
-     *
-     * @access protected
-     * @param string $query SQL query
-     * @param array $params Query parameters
-     * @return array
-     */
-
-    protected function prepare($query, array $params)
+    
+    public function query($query)
     {
-        // Replace IN clause placeholder with escaped values
-        replace:
-        if(strpos($query, '([?])') !== false)
+        $this->queries[] = $query;
+    }
+    
+    public function queries()
+    {
+        return $this->queries;
+    }
+    
+    public function username($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+    
+    public function password($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+    
+    public function property($name, $value)
+    {
+        $this->properties[$name] = $value;
+        return $this;
+    }
+    
+    public function options(array $options)
+    {
+        foreach($options as $name => $value)
         {
-            foreach($params as $key => $value)
-            {
-                if(is_array($value))
+            $this->option($name, $value);
+        }
+        
+        return $this;
+    }
+    
+    public function option($name, $value)
+    {
+        $this->options[$name] = $value;
+        return $this;
+    }
+    
+    public function pdo()
+    {
+        return new PDO($this->prefix . ':' . implode(';', $this->properties), $this->username, $this->password, $this->options);
+    }
+    
+    protected function compiler()
+    {
+        switch($this->prefix)
+        {
+            case 'mysql':
+                return new \Opis\Database\Compiler\MySQL();
+            case 'dblib':
+            case 'mssql':
+            case 'sqlsrv':
+            case 'sybase':
+                return new \Opis\Database\Compiler\SQLServer();
+            case 'oci':
+            case 'oracle':
+                return new \Opis\Database\Compiler\Oracle();
+            case 'firebird':
+                return new \Opis\Database\Compiler\Firebird();
+            case 'db2':
+            case 'ibm':
+            case 'odbc':
+                return new \Opis\Database\Compiler\DB2();
+            case 'nuodb':
+                return new \Opis\Database\Compiler\NuoDB();
+            default:
+                if(isset(static::$compilers[$this->prefix]))
                 {
-                    array_splice($params, $key, 1, $value);
-                    $query = preg_replace('/\(\[\?\]\)/', '(' . trim(str_repeat('?, ', count($value)), ', ') . ')', $query, 1);
-                    goto replace;
+                    return static::$compilers[$this->prefix]();
                 }
+        }
+        return new \Opis\Database\SQL\Compiler();
+    }
+    
+    public static function registerCompiler($prefix, Closure $closure)
+    {
+        static::$compilers[$prefix] = $closure;
+    }
+    
+    public static function get($name = null)
+    {
+        if($name == null)
+        {
+            if(static::$defaultConnection != null)
+            {
+                return static::$connections[static::$defaultConnection];
             }
+            return reset(static::$connections);
         }
-        // Prepare statement
-        try
+        return static::$connections[$name];
+    }
+    
+    public static function other($prefix, $name, $default = false)
+    {
+        $connection = new Connection($prefix);
+        static::$connections[$name] = $connection;
+        if($default === true)
         {
-            $statement = $this->pdo->prepare($query);
-        }catch(PDOException $e)
-        {
-            throw new PDOException($e->getMessage() . ' [ ' . $this->replaceParams($query, $params) . ' ] ', (int) $e->getCode(), $e->getPrevious());
+            static::$defaultConnection = $name;
         }
-        // Return query, parameters and the prepared statement
-        return array('query' => $query, 'params' => $params, 'statement' => $statement);
+        return $connection;
     }
-
-    /**
-     * Executes the prepared query and returns TRUE on success or FALSE on failure.
-     *
-     * @access protected
-     * @param array $prepared Prepared query
-     * @return boolean
-     */
-
-    protected function execute(array $prepared)
+    
+    
+    public static function dblib($name, $default = false)
     {
-        if($this->enableLog)
-        {
-            $start = microtime(true);
-        }
-        $result = $prepared['statement']->execute($prepared['params']);
-        if($this->enableLog)
-        {
-            $this->log($prepared['query'], $prepared['params'], $start);
-        }
-        return $result;
+        return static::other('dblib', $name, $default);
     }
-
-    /**
-     * Executes the query and returns TRUE on success or FALSE on failure.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return mixed
-     */
-
-    public function query($query, array $params = array())
+    
+    public static function sybase($name, $default = false)
     {
-        return $this->execute($this->prepare($query, $params));
+        return static::other('sybase', $name, $default);
     }
-
-    /**
-     * Executes the query and returns TRUE on success or FALSE on failure.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return boolean
-     */
-
-    public function insert($query, array $params = array())
+    
+    public static function mssql($name, $default = false)
     {
-        return $this->query($query, $params);
+        return static::other('mssql', $name, $default);
     }
-
-    /**
-     * Returns an array containing all of the result set rows.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return array
-     */
-
-    public function all($query, array $params = array())
+    
+    public static function firebird($name, $default = false)
     {
-        $prepared = $this->prepare($query, $params);
-        $this->execute($prepared);
-        return $prepared['statement']->fetchAll();
+        return static::other('firebird', $name, $default);
     }
-
-    /**
-     * Returns the first row of the result set.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query params
-     * @return mixed
-     */
-
-    public function first($query, array $params = array())
+    
+    public static function ibm($name, $default = false)
     {
-        $prepared = $this->prepare($query, $params);
-        $this->execute($prepared);
-        return $prepared['statement']->fetch();
+        return static::other('ibm', $name, $default);
     }
-
-    /**
-     * Returns the value of the first column of the first row of the result set.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return mixed
-     */
-
-    public function column($query, array $params = array())
+    
+    public static function mysql($name, $default = false)
     {
-        $prepared = $this->prepare($query, $params);
-        $this->execute($prepared);
-        return $prepared['statement']->fetchColumn();
+        return static::other('mysql', $name, $default);
     }
-
-    /**
-     * Executes the query and return number of affected rows.
-     *
-     * @access protected
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return int
-     */
-
-    protected function executeAndCount($query, array $params)
+    
+    public static function sqlsrv($name, $default = false)
     {
-        $prepared = $this->prepare($query, $params);
-        $this->execute($prepared);
-        return $prepared['statement']->rowCount();
+        return static::other('sqlsrv', $name, $default);
     }
-
-    /**
-     * Executes the query and returns the number of updated records.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return int
-     */
-
-    public function update($query, array $params = array())
+    
+    public static function oci($name, $default = false)
     {
-        return $this->executeAndCount($query, $params);
+        return static::other('oci', $name, $default);
     }
-
-    /**
-     * Executes the query and returns the number of deleted records.
-     *
-     * @access public
-     * @param string $query SQL query
-     * @param array $params (optional) Query parameters
-     * @return int
-     */
-
-    public function delete($query, array $params = array())
+    
+    public static function odbc($name, $default = false)
     {
-        return $this->executeAndCount($query, $params);
+        return static::other('odbc', $name, $default);
     }
-
-    /**
-     * Returns a query builder instance.
-     *
-     * @access public
-     * @param mixed $table Table name or subquery
-     * @return \Opis\Database\SQL\Query
-     */
-
-    public function table($table)
+    
+    public static function pgsql($name, $default = false)
     {
-        return new Query($this, $table);
+        return static::other('pgsql', $name, $default);
     }
-
-    /**
-     * Executes queries and rolls back the transaction if any of them fail.
-     *
-     * @access public
-     * @param \Closure $queries Queries
-     * @return mixed
-     */
-
-    public function transaction(Closure $queries)
+    
+    public static function sqlite($name, $default = false)
     {
-        try
-        {
-            $this->pdo->beginTransaction();
-            $result = $queries($this);
-            $this->pdo->commit();
-            
-        }catch(PDOException $e)
-        {
-            $this->pdo->rollBack();
-            throw $e;
-        }
-        return $result;
+        return static::other('sqlite', $name, $default);
+    }
+    
+    public static function nuodb($name, $default = false)
+    {
+        return static::other('nuodb', $name, $default);
     }
 }
