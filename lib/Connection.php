cd45bb177bec7ae5f126021c772387d4d6e80884
msarca <marius.sarca@gmail.com>
2014-07-04 18:24:21 +0300

    Changed Connection class

--- lib/Connection.php
+++ lib/Connection.php
@@ -25,23 +25,14 @@ use PDO;
 use PDOException;
 use RuntimeException;
 use Serializable;
-use Opis\Database\DSN\Generic as GenericConnection;
-use Opis\Database\DSN\MySQL as MySQLConnection;
-use Opis\Database\DSN\IBM as IBMConnection;
-use Opis\Database\DSN\Oracle as OracleConnection;
-use Opis\Database\DSN\PostgreSQL as PostgreSQLConnection;
-use Opis\Database\DSN\Firebird as FirebirdConnection;
-use Opis\Database\DSN\SQLite as SQLiteConnection;
-use Opis\Database\DSN\SQLServer as SQLServerConnection;
-use Opis\Database\DSN\DBLib as DBLibConnection;
 
 class Connection implements Serializable
 {
     /** @var    string  Username */
-    protected $username = null;
+    protected $username;
     
     /** @var    string  Password */
-    protected $password = null;
+    protected $password;
     
     /** @var    bool    Log queries flag */
     protected $logQueries = false;
@@ -60,61 +51,30 @@ class Connection implements Serializable
         PDO::ATTR_EMULATE_PREPARES => false,
     );
     
-    /** @var    string  DSN prefix */
-    protected $prefix;
-    
-    /** @var    string  Database */
-    protected $database;
-    
-    /** @var    array   DSN properties */
-    protected $properties = array();
-    
     /** @var    \PDO    The PDO object associated with this connection */
-    protected $pdo = null;
+    protected $pdo;
     
     /** @var    \Opis\Database\Compiler The compiler associated with this connection */
-    protected $compiler = null;
+    protected $compiler;
     
-    /** @var    string  The DSN assocatied with this connection */
-    protected $dsn = null;
+    /** @var    string  The DSN for this connection */
+    protected $dsn;
     
     /**
      * Constructor
      * 
      * @access public
      *
-     * @param string $prefix    DSN prefix
-     * @param string $username  (optional) Username
-     * @param string $password  (optional) Password
+     * @param   string  $dsn    The DSN string
+     * @param   string  $username  (optional) Username
+     * @param   string  $password  (optional) Password
      */
     
-    public function __construct($prefix, $username = null, $password = null)
+    public function __construct($dsn, $username = null, $password = null)
     {
-        $this->prefix = $prefix;
+        $this->dsn = $dsn;
         $this->username = $username;
         $this->password = $password;
-    }
-    
-    /**
-     * Returns the database name for this connection
-     *
-     * @return string
-     */
-    
-    public function dbname()
-    {
-        return $this->database;
-    }
-    
-    /**
-     * Returns the DSN prefix
-     *
-     * @return string
-     */
-    
-    public function prefix()
-    {
-        return $this->prefix;
     }
     
     /**
@@ -179,35 +139,6 @@ class Connection implements Serializable
         return $this;
     }
     
-    /**
-     * Set a DSN property
-     *
-     * @param   string  $name   Property name
-     * @param   string  $value  Property value
-     *
-     * @return  \Opis\Database\Connection
-     */
-    
-    public function set($name, $value)
-    {
-        $this->properties[$name] = $value;
-        return $this;
-    }
-    
-    /**
-     * Set the database DSN property
-     *
-     * @param   string  $name   DSN property
-     * @param   string  $value  Database name
-     *
-     * @return  \Opis\Database\Connection
-     */
-    
-    public function setDatabase($name, $value)
-    {
-        $this->database = $value;
-        return $this->set($name, $value);
-    }
     
     /**
      * Set PDO connection options
@@ -223,11 +154,12 @@ class Connection implements Serializable
         {
             $this->option($name, $value);
         }
+        
         return $this;
     }
     
     /**
-     * Set a connection option
+     * Set a PDO connection option
      *
      * @param   string  $name   Option
      * @param   int     $value  Value
@@ -242,20 +174,9 @@ class Connection implements Serializable
     }
     
     /**
-     * Set charset
-     *
-     * @param   string  $value  Charset
+     * Use persistent connections
      *
      * @return  \Opis\Database\Connection
-     */
-    
-    public function charset($value)
-    {
-        return $this;
-    }
-    
-    /**
-     * Use persistent connections
      */
     
     public function persistent()
@@ -264,27 +185,18 @@ class Connection implements Serializable
     }
     
     /**
-     * Generate the DSN associated with this connection
+     * Returns the DSN associated with this connection
      *
      * @return  string
      */
     
     public function dsn()
     {
-        if($this->dsn === null)
-        {
-            $tmp = array();
-            foreach($this->properties as $key => $value)
-            {
-                $tmp[] = $key . '=' . $value;
-            }
-            $this->dsn = $this->prefix() . ':' . implode(';', $tmp);
-        }
         return $this->dsn;
     }
     
     /**
-     * Construct the PDO object associated with this connection
+     * Returns the PDO object associated with this connection
      *
      * @return \PDO
      */
@@ -293,16 +205,7 @@ class Connection implements Serializable
     {
         if($this->pdo == null)
         {
-            try
-            {
-                
-                $this->pdo = new PDO($this->dsn(), $this->username, $this->password, $this->options);
-                
-            }
-            catch(PDOException $e)
-            {
-                throw new RuntimeException(vsprintf("%s(): Failed to connect to the '%s' database. %s", array(__METHOD__, $this->dbname(), $e->getMessage())));
-            }
+            $this->pdo = new PDO($this->dsn(), $this->username, $this->password, $this->options);
             
             foreach($this->commands as $command)
             {
@@ -324,7 +227,35 @@ class Connection implements Serializable
     {
         if($this->compiler === null)
         {
-            $this->compiler = new \Opis\Database\SQL\Compiler();
+            switch($this->pdo()->getAttribute(PDO::ATTR_DRIVER_NAME))
+            {
+                case 'mysql':
+                    $this->compiler = new \Opis\Database\Compiler\MySQL();
+                    break;
+                case 'dblib':
+                case 'mssql':
+                case 'sqlsrv':
+                case 'sybase':
+                    $this->compiler = new \Opis\Database\Compiler\SQLServer();
+                    break;
+                case 'oci':
+                case 'oracle':
+                    $this->compiler = new \Opis\Database\Compiler\Oracle();
+                    break;
+                case 'firebird':
+                    $this->compiler = new \Opis\Database\Compiler\Firebird();
+                    break;
+                case 'db2':
+                case 'ibm':
+                case 'odbc':
+                    $this->compiler = new \Opis\Database\Compiler\DB2();
+                    break;
+                case 'nuodb':
+                    $this->compiler = new \Opis\Database\Compiler\NuoDB();
+                    break;
+                default:
+                    $this->compiler = new \Opis\Database\SQL\Compiler();
+            }
         }
         
         return $this->compiler;
@@ -527,16 +458,14 @@ class Connection implements Serializable
             'logQueries' => $this->logQueries,
             'options' => $this->options,
             'queries' => $this->queries,
-            'properties' => $this->properties,
-            'prefix' => $this->prefix,
-            'database' => $this->database,
-            'dsn' => $this->dsn(),
+            'dsn' => $this->dsn,
         ));
     }
     
     public function unserialize($data)
     {
         $object = unserialize($data);
+        
         foreach($object as $key => $value)
         {
             $this->{$key} = $value;
@@ -544,146 +473,18 @@ class Connection implements Serializable
     }
     
     /**
-     * Creates a generic connection
+     * Creates a new connection
      *
      * @param   string  $dsn        DSN connection string
      * @param   string  $username   (optional) Username
      * @param   string  $password   (optional)  Password
      *
-     * @return  \Opis\Database\DSN\Generic
+     * @return  \Opis\Database\Connection
      */
     
-    public static function generic($dsn, $username = null, $password = null)
+    public static function create($dsn, $username = null, $password = null)
     {
-        return new GenericConnection($dsn, $username, $password);
+        return new static($dsn, $username, $password);
     }
-    
-    /**
-     * Creates a Firebird specific connection
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis\Database\DSN\Firebird
-     */
-    
-    public static function firebird($username, $password)
-    {
-        return new FirebirdConnection($username, $password);
-    }
-    
-    /**
-     * Creates an IBM specific connection
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis\Database\DSN\IBM
-     */
-    
-    public static function ibm($username, $password)
-    {
-        return new IBMConnection($username, $password);
-    }
-    
-    /**
-     * Creates an Oracle specific connection
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis\Database\DSN\Oracle
-     */
-    
-    public static function oracle($username, $password)
-    {
-        return new OracleConnection($username, $password);
-    }
-    
-    /**
-     * Creates a MySQL specific connection
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis\Database\DSN\MySQL
-     */
-    
-    public static function mysql($username, $password)
-    {
-        return new MySQLConnection($username, $password);
-    }
-    
-    /**
-     * Creates a PostgreSQL specific connection
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis\Database\DSN\PostgreSQL
-     */
-    
-    public static function postgreSQL($username, $password)
-    {
-        return new PostgreSQLConnection($username, $password);
-    }
-    
-    /**
-     * Connecting to an Microsoft SQL Server and SQL Azure database
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     *
-     * @return  \Opis|Database\DSN\SQLServer
-     */
-    
-    public static function sqlServer($username, $password)
-    {
-        return new SQLServerConnection($username, $password);
-    }
-    
-    /**
-     * Connecting to an Microsoft SQL Server or Sybase database
-     *
-     * @param   string  $username   Username
-     * @param   string  $password   Password
-     * @param   string  $driver     (Optional) Driver
-     * 
-     * @return  \Opis|Database\DSN\DBLib
-     */
-    
-    public static function mssql($username, $password, $driver = 'dblib')
-    {
-        return new DBLibConnection($driver, $username, $password);
-    }
-    
-    /**
-     * Creates an SQLite specific connection
-     *
-     * If the $path parameter is omitted, an in-memory database will be created
-     * 
-     * @param   string  $path   (optional) Path to database
-     *
-     * @return  \Opis\Database\DSN\SQLite
-     */
-    
-    public static function sqlite($path = null)
-    {
-        return new SQLiteConnection($path);
-    }
-    
-    /**
-     * Creates an SQLite2 specific connection
-     *
-     * If the $path parameter is omitted, an in-memory database will be created
-     * 
-     * @param   string  $path   (optional) Path to database
-     *
-     * @return  \Opis\Database\DSN\SQLite
-     */
-    
-    public static function sqlite2($path = null)
-    {
-        return new SQLiteConnection($path, '2');
-    }
+
 }
