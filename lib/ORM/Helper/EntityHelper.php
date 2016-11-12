d6ddbda71fe09c61106a109c64d810f3df738d91
Marius Sarca <marius.sarca@gmail.com>
2016-11-12 23:07:51 +0200

    Added helper classes

---
+++ lib/ORM/Helper/EntityHelper.php
@@ -0,0 +1,33 @@
+<?php
+/* ===========================================================================
+ * Copyright 2013-2016 The Opis Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================================ */
+
+namespace Opis\Database\ORM\Helper;
+
+use Opis\Database\Entity;
+use Opis\Database\ORM\DataMapper;
+
+class EntityHelper extends Entity
+{
+    /**
+     * @param Entity $entity
+     * @return DataMapper
+     */
+    public static function getDataMapper(Entity $entity): DataMapper
+    {
+        return $entity->orm();
+    }
+}
