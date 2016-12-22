042977ab8209fca65046962d18290a8744f5ebd6
Marius Sarca <marius.sarca@gmail.com>
2016-12-22 11:29:38 +0200

    Renamed 'lib' to 'src'

--- autoload.php
+++ autoload.php
@@ -21,7 +21,7 @@
 spl_autoload_register(function ($class) {
 
     $class = ltrim($class, '\\');
-    $dir = __DIR__ . '/lib';
+    $dir = __DIR__ . '/src';
     $namespace = 'Opis\Database';
 
     if (strpos($class, $namespace) === 0) {
